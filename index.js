4e52e184d49f63c1d79eaa526fa29f74c3cac8d8
Anthony Rodriguez <rdz.tony1@gmail.com>
2022-08-13 18:36:22 -0500

    commit tree 0dcc8558eafea5319e902d155e1848a8e1f76a5f

---
+++ index.js
@@ -0,0 +1,15 @@
+function receivesAFunction(name) {
+    name();
+}
+
+const returnsANamedFunction=function() {
+    return  returnsANamedFunction;
+}
+
+
+function returnsAnAnonymousFunction() {
+    return function() {
+        '4+5'
+    }
+}
+
